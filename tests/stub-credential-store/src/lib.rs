42ab2fa35acc0024c1b1c736beb13a7d62056580
Scott Andrews <scott@andrews.me>
2025-05-14 12:15:53 -0400

    Allow service instances to use credential store
    The credential-store and credential-admin interfaces were originally
    intended to store credentials for service bindings. Some lifecycles also
    need to interact with credentials for service instances. Now service
    instance and bindings IDs can be used with the credential store.
    
    This pattern was previously used within the test components. Now it is
    formally allowed.
    
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- tests/stub-credential-store/src/lib.rs
+++ tests/stub-credential-store/src/lib.rs
@@ -1,19 +1,13 @@
 #![no_main]
 
-use exports::componentized::services::credential_store::{
-    Credential, Error, Guest, ServiceBindingId,
-};
+use exports::componentized::services::credential_store::{Credential, Error, Guest, ServiceId};
 use wasi::logging::logging::{log, Level};
 
 pub(crate) struct StubCredentialStore;
 
 impl Guest for StubCredentialStore {
-    fn fetch(binding_id: ServiceBindingId) -> Result<Vec<Credential>, Error> {
-        log(
-            Level::Info,
-            "credential-store",
-            &format!("fetch binding-id={binding_id}"),
-        );
+    fn fetch(id: ServiceId) -> Result<Vec<Credential>, Error> {
+        log(Level::Info, "credential-store", &format!("fetch id={id}"));
         Ok(vec![])
     }
 }
